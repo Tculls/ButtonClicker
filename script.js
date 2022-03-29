a8ba5f9cc5b72e33e5a19a634d0be2ec0a8a876a
Tculls <tanner.culley@gmail.com>
2022-03-28 18:57:59 -0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function likedDefinition(alert){
+    console.log(alert);
+}
+function change(){
+    console.log("click");
+    document.getElementById("LogInbtn").value="Log-Out";
+
+}
+function addWord(element){
+    element.remove();
+}
